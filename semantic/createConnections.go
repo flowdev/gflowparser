2f6671d94cd0ddb0ca747552156eb86a657a6949
sb_olebulbuk <ole@sensorberg.com>
2016-02-19 10:21:08 +0100

    Some preparations for semantics and a sample HTML output

--- semantic/createConnections.go
+++ semantic/createConnections.go
@@ -1,45 +1,42 @@
-package semantic
+package main
 
-import (
-//	"github.com/flowdev/gflowparser/parser"
-)
 
 type SemanticConnections struct {
-	//	createConns     *CreateConnections
-	//	verifyOutPorts  *VerifyOutPortsUsedOnlyOnce
-	//	handleChainBeg  *HandleChainBeg
-	//	handleChainMids *HandleChainMids
-	//	handleChainEnd  *HandleChainEnd
-	//	begAddLastOp    *AddLastOp
-	//	midAddLastOp    *AddLastOp
-	InPort func(interface{})
+ createConns *CreateConnections
+ verifyOutPorts *VerifyOutPortsUsedOnlyOnce
+ handleChainBeg *HandleChainBeg
+ handleChainMids *HandleChainMids
+ handleChainEnd *HandleChainEnd
+ begAddLastOp *AddLastOp
+ midAddLastOp *AddLastOp
+	InPort func(ParserData)
 }
 
 func NewSemanticConnections() *SemanticConnections {
+    f := &SemanticConnections{}
+ f.createConns = NewCreateConnections()
+ f.verifyOutPorts = NewVerifyOutPortsUsedOnlyOnce()
+ f.handleChainBeg = NewHandleChainBeg()
+ f.handleChainMids = NewHandleChainMids()
+ f.handleChainEnd = NewHandleChainEnd()
+ f.begAddLastOp = NewAddLastOp()
+ f.midAddLastOp = NewAddLastOp()
 
-	f := &SemanticConnections{}
-	//	f.createConns = NewCreateConnections()
-	//	f.verifyOutPorts = NewVerifyOutPortsUsedOnlyOnce()
-	//	f.handleChainBeg = NewHandleChainBeg()
-	//	f.handleChainMids = NewHandleChainMids()
-	//	f.handleChainEnd = NewHandleChainEnd()
-	//	f.begAddLastOp = NewAddLastOp()
-	//	f.midAddLastOp = NewAddLastOp()
+    f.createConns.SetOutPort(f.verifyOutPorts.InPort)
+    f.createConns.SetChainOutPort(f.handleChainBeg.InPort)
+    f.handleChainBeg.SetOutPort(f.handleChainMids.InPort)
+    f.handleChainMids.SetOutPort(f.handleChainEnd.InPort)
+    f.handleChainEnd.SetOutPort(f.createConns.ChainInPort)
+    f.handleChainBeg.SetAddOpOutPort(f.begAddLastOp.InPort)
+    f.begAddLastOp.SetOutPort(f.handleChainBeg.AddOpInPort)
+    f.handleChainMids.SetAddOpOutPort(f.midAddLastOp.InPort)
+    f.midAddLastOp.SetOutPort(f.handleChainMids.AddOpInPort)
 
-	//	f.createConns.SetOutPort(f.verifyOutPorts.InPort)
-	//	f.createConns.SetChainOutPort(f.handleChainBeg.InPort)
-	//	f.handleChainBeg.SetOutPort(f.handleChainMids.InPort)
-	//	f.handleChainMids.SetOutPort(f.handleChainEnd.InPort)
-	//	f.handleChainEnd.SetOutPort(f.createConns.ChainInPort)
-	//	f.handleChainBeg.SetAddOpOutPort(f.begAddLastOp.InPort)
-	//	f.begAddLastOp.SetOutPort(f.handleChainBeg.AddOpInPort)
-	//	f.handleChainMids.SetAddOpOutPort(f.midAddLastOp.InPort)
-	//	f.midAddLastOp.SetOutPort(f.handleChainMids.AddOpInPort)
+	f.InPort = f.createConns.InPort
 
-	//	f.InPort = f.createConns.InPort
-
-	return f
+    return f
 }
-func (f *SemanticConnections) SetOutPort(port func(interface{})) {
-	//	f.verifyOutPorts.SetOutPort(port)
+func (f *SemanticConnections) SetOutPort(port func()) {
+	f.verifyOutPorts.SetOutPort(port)
 }
+
