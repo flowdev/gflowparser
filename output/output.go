6c549b0914cf1fef26dce6ad114c4cba4d71e185
ole108 <ole@bulbuk.de>
2017-04-09 14:26:57 +0200

    FillPortPairs should work now.

--- output/output.go
+++ output/output.go
@@ -4,6 +4,10 @@ import (
 	"github.com/flowdev/gflowparser/data"
 )
 
+var AllowedFormat = map[string]bool{
+	"dot": true,
+	"go":  true,
+}
 var specialFormat = map[string]bool{
 	"dot": true,
 }
@@ -34,4 +38,49 @@ func (op *ProduceFormats) SetOutPort(port func(*data.MainData)) {
 }
 func (op *ProduceFormats) SetSpecialOutPort(port func(*data.MainData)) {
 	op.specialOutPort = port
+}
+
+// ------------ FillPortPairs:
+// input:  *data.MainData{SelectedFormats, FlowFile}
+// output: *data.MainData{SelectedFormats, FlowFile, CurrentFormat} to the correct output port.
+type FillPortPairs struct {
+	outPort func(*data.MainData)
+}
+
+func NewFillPortPairs() *FillPortPairs {
+	return &FillPortPairs{}
+}
+func (op *FillPortPairs) InPort(md *data.MainData) {
+	for _, flow := range md.FlowFile.Flows {
+		for _, op := range flow.Ops {
+			fillPortPairs4Op(op)
+		}
+	}
+	op.outPort(md)
+}
+func (op *FillPortPairs) SetOutPort(port func(*data.MainData)) {
+	op.outPort = port
+}
+
+func fillPortPairs4Op(op *data.Operation) {
+	l := len(op.InPorts)
+	m := len(op.OutPorts)
+	n := max(l, m)
+	portPairs := make([]*data.PortPair, n)
+	for i := 0; i < n; i++ {
+		p := &data.PortPair{}
+		if i < l {
+			p.InPort = op.InPorts[i]
+		}
+		if i < m {
+			p.OutPort = op.OutPorts[i]
+		}
+		portPairs[i] = p
+	}
+}
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
 }
