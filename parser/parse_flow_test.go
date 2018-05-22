69b3bc4ed62416f9f6b828476b987b30fe5313f0
Ole Bulbuk <ole.bulbuk@optiopay.com>
2018-05-22 14:31:48 +0200

    Test ParsePort

---
+++ parser/parse_flow_test.go
@@ -0,0 +1,62 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/flowdev/gflowparser/data"
+)
+
+func TestParsePort(t *testing.T) {
+	p, err := NewParsePort()
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+	runTests(t, p.In, []parseTestData{
+		{
+			givenName:        "empty",
+			givenContent:     ``,
+			expectedValue:    nil,
+			expectedErrCount: 1,
+		}, {
+			givenName:        "no match 1",
+			givenContent:     `1:a`,
+			expectedValue:    nil,
+			expectedErrCount: 1,
+		}, {
+			givenName:        "no match 2",
+			givenContent:     `_a`,
+			expectedValue:    nil,
+			expectedErrCount: 1,
+		}, {
+			givenName:        "simple 1",
+			givenContent:     `aB`,
+			expectedValue:    data.Port{Name: "aB"},
+			expectedErrCount: 0,
+		}, {
+			givenName:        "simple 2",
+			givenContent:     `a0`,
+			expectedValue:    data.Port{Name: "a0"},
+			expectedErrCount: 0,
+		}, {
+			givenName:        "simple 3",
+			givenContent:     `aB_cd`,
+			expectedValue:    data.Port{Name: "aB"},
+			expectedErrCount: 0,
+		}, {
+			givenName:        "simple 4",
+			givenContent:     `abcDef`,
+			expectedValue:    data.Port{Name: "abcDef"},
+			expectedErrCount: 0,
+		}, {
+			givenName:        "complex 1",
+			givenContent:     `ab1Cd:1`,
+			expectedValue:    data.Port{Name: "ab1Cd", HasIndex: true, Index: 1},
+			expectedErrCount: 0,
+		}, {
+			givenName:        "complex 2",
+			givenContent:     `a1Bc:003`,
+			expectedValue:    data.Port{Name: "a1Bc", HasIndex: true, Index: 3},
+			expectedErrCount: 0,
+		},
+	})
+}
