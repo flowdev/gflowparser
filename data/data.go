18f6097c54ef48a5f8ab5270e63fcd9e5ab98a00
Ole Bulbuk <ole.bulbuk@optiopay.com>
2018-05-23 22:07:40 +0200

    Add untested ParseFlow

--- data/data.go
+++ data/data.go
@@ -1,61 +1,8 @@
 package data
 
-import (
-	"strings"
-)
-
-type PortData struct {
-	Name     string
-	CapName  string
-	HasIndex bool
-	Index    int
-	SrcPos   int
-}
-
-func NewPort(name string, srcPos int) *PortData {
-	return newPort(name, false, 0, srcPos)
-}
-func NewIdxPort(name string, idx int, srcPos int) *PortData {
-	return newPort(name, true, idx, srcPos)
-}
-func CopyPort(port *PortData, srcPos int) *PortData {
-	return &PortData{port.Name, port.CapName, port.HasIndex, port.Index, srcPos}
-}
-func DefaultInPort(srcPos int) *PortData {
-	return NewPort("in", srcPos)
-}
-func DefaultOutPort(srcPos int) *PortData {
-	return NewPort("out", srcPos)
-}
-func newPort(name string, hasIdx bool, idx int, srcPos int) *PortData {
-	capName := name
-	if len(name) > 0 {
-		capName = strings.ToUpper(name[0:1]) + name[1:]
-	}
-	return &PortData{name, capName, hasIdx, idx, srcPos}
-}
-
-type PortPair struct {
-	InPort  *PortData
-	OutPort *PortData
-}
-
-type Operation struct {
-	Name      string
-	Type      string
-	InPorts   []*PortData
-	OutPorts  []*PortData
-	SrcPos    int
-	PortPairs []*PortPair
-}
-
-type Connection struct {
-	FromOp       *Operation
-	FromPort     *PortData
-	DataType     string
-	ShowDataType bool
-	ToOp         *Operation
-	ToPort       *PortData
+// Flow is the semantic representation of a complete flow.
+type Flow struct {
+	Parts [][]interface{}
 }
 
 // Arrow is the semantic representation of a flow arrow including data type and
--- parser/parse_flow.go
+++ parser/parse_flow.go
@@ -44,8 +44,7 @@ func (p *ParsePort) In(pd *gparselib.ParseData, ctx interface{}) (*gparselib.Par
 		return pd2, ctx2
 	}
 	pIndex := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
-		return gparselib.ParseAll(
-			pd, ctx,
+		return gparselib.ParseAll(pd, ctx,
 			[]gparselib.SubparserOp{pColon, pNumber},
 			func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
 				pd2.Result.Value = pd2.SubResults[1].Value
@@ -56,8 +55,7 @@ func (p *ParsePort) In(pd *gparselib.ParseData, ctx interface{}) (*gparselib.Par
 	pOpt := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
 		return gparselib.ParseOptional(pd2, ctx2, pIndex, nil)
 	}
-	return gparselib.ParseAll(
-		pd, ctx,
+	return gparselib.ParseAll(pd, ctx,
 		[]gparselib.SubparserOp{p.pName.In, pOpt},
 		parsePortSemantic,
 	)
@@ -78,6 +76,23 @@ func parsePortSemantic(pd *gparselib.ParseData, ctx interface{}) (*gparselib.Par
 // ParseArrow parses a flow arrow including ports and data types.
 // Semantic result: data.Arrow
 //
+// flow:
+//     in (ParseData)-> [pOptPort gparselib.ParseOptional [ParsePort]] -> out
+//     in (ParseData)-> [pLeftParen gparselib.ParseLiteral] -> out
+//     in (ParseData)-> [pRightParen gparselib.ParseLiteral] -> out
+//     in (ParseData)-> [pArrow gparselib.ParseLiteral] -> out
+//     in (ParseData)-> [pData gparselib.ParseAll
+//                          [pLeftParen, ParseOptSpc,
+//                           ParseTypeList, ParseOptSpc,
+//                           pRightParen, ParseOptSpc
+//                          ]
+//                      ] -> out
+//     in (ParseData)-> [pOptData gparselib.ParseOptional [pData]] -> out
+//     in (ParseData)-> [gparselib.ParseAll
+//                          [pOptPort, ParseSpaceComment, pOptData,
+//                           pArrow, ParseSpaceComment, pOptPort
+//                          ]
+//                      ] -> out
 //
 // Details:
 type ParseArrow struct {
@@ -101,23 +116,6 @@ func NewParseArrow() (*ParseArrow, error) {
 }
 
 // In is the input port of the ParsePort operation.
-//
-// flow:
-//     in (ParseData)-> [pOptPort gparselib.ParseOptional [ParsePort]] -> out
-//     in (ParseData)-> [pLeftParen gparselib.ParseLiteral] -> out
-//     in (ParseData)-> [pRightParen gparselib.ParseLiteral] -> out
-//     in (ParseData)-> [pArrow gparselib.ParseLiteral] -> out
-//     in (ParseData)-> [pData gparselib.ParseAll
-//                          [pLeftParen, ParseOptSpc,
-//                           ParseTypeList, ParseOptSpc,
-//                           pRightParen, ParseOptSpc
-//                          ]
-//                      ] -> out
-//     in (ParseData)-> [pOptData gparselib.ParseOptional [pData]] -> out
-//     in (ParseData)-> [gparselib.ParseAll
-//                          [pOptPort, ParseSpaceComment, pOptData,
-//                           pArrow, ParseSpaceComment, pOptPort]
-//                      ] -> out
 func (p *ParseArrow) In(pd *gparselib.ParseData, ctx interface{}) (*gparselib.ParseData, interface{}) {
 	pOptPort := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
 		return gparselib.ParseOptional(pd2, ctx2, p.pPort.In, nil)
@@ -132,8 +130,7 @@ func (p *ParseArrow) In(pd *gparselib.ParseData, ctx interface{}) (*gparselib.Pa
 		return gparselib.ParseLiteral(pd, ctx, nil, `->`)
 	}
 	pData := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
-		return gparselib.ParseAll(
-			pd, ctx,
+		return gparselib.ParseAll(pd, ctx,
 			[]gparselib.SubparserOp{
 				pLeftParen, ParseOptSpc,
 				p.pData.In, ParseOptSpc,
@@ -149,8 +146,7 @@ func (p *ParseArrow) In(pd *gparselib.ParseData, ctx interface{}) (*gparselib.Pa
 		return gparselib.ParseOptional(pd2, ctx2, pData, nil)
 	}
 
-	return gparselib.ParseAll(
-		pd, ctx,
+	return gparselib.ParseAll(pd, ctx,
 		[]gparselib.SubparserOp{
 			pOptPort, ParseSpaceComment, pOptData,
 			pArrow, ParseSpaceComment, pOptPort,
@@ -175,5 +171,107 @@ func parseArrowSemantic(pd *gparselib.ParseData, ctx interface{}) (*gparselib.Pa
 		arrow.ToPort = &port
 	}
 	pd.Result.Value = arrow
+	return pd, ctx
+}
+
+// ParseFlow parses a complete flow.
+// Semantic result: data.Flow
+//
+// flow:
+//     in (ParseData)-> [pOptArrow gparselib.ParseOptional [ParseArrow]] -> out
+//     in (ParseData)-> [pPair gparselib.ParseAll
+//                          [ParseComponent, ParseOptSpc,
+//                           ParseArrow, ParseOptSpc
+//                          ]
+//                      ] -> out
+//     in (ParseData)-> [pPairs gparselib.ParseMulti1 [pPair]] -> out
+//     in (ParseData)-> [pPartLine gparselib.ParseAll
+//                          [pOptArrow, ParseOptSpc, pPairs, ParseSpaceComment]
+//                      ] -> out
+//     in (ParseData)-> [gparselib.ParseMulti1 [pPartLine]] -> out
+//
+// Details:
+type ParseFlow struct {
+	pArrow *ParseArrow
+	pComp  *ParseComponent
+}
+
+// NewParseFlow creates a new parser for a flow.
+// If any regular expression used by the subparsers is invalid an error is
+// returned.
+func NewParseFlow() (*ParseFlow, error) {
+	pArrow, err := NewParseArrow()
+	if err != nil {
+		return nil, err
+	}
+	pComp, err := NewParseComponent()
+	if err != nil {
+		return nil, err
+	}
+	return &ParseFlow{pArrow: pArrow, pComp: pComp}, nil
+}
+
+// In is the input port of the ParsePort operation.
+func (p *ParseFlow) In(pd *gparselib.ParseData, ctx interface{}) (*gparselib.ParseData, interface{}) {
+	pOptArrow := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
+		return gparselib.ParseOptional(pd2, ctx2, p.pArrow.In, nil)
+	}
+	pPair := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
+		return gparselib.ParseAll(pd, ctx,
+			[]gparselib.SubparserOp{
+				p.pComp.In, ParseOptSpc,
+				p.pArrow.In, ParseOptSpc,
+			},
+			func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
+				pd2.Result.Value = []interface{}{
+					pd2.SubResults[0].Value,
+					pd2.SubResults[2].Value,
+				}
+				return pd2, ctx2
+			},
+		)
+	}
+	pPairs := func(pd *gparselib.ParseData, ctx interface{}) (*gparselib.ParseData, interface{}) {
+		return gparselib.ParseMulti1(pd, ctx, pPair, parsePairsSemantic)
+	}
+	pPartLine := func(pd2 *gparselib.ParseData, ctx2 interface{}) (*gparselib.ParseData, interface{}) {
+		return gparselib.ParseAll(pd, ctx,
+			[]gparselib.SubparserOp{
+				pOptArrow, ParseOptSpc, pPairs, ParseSpaceComment,
+			},
+			parsePartLineSemantic,
+		)
+	}
+	return gparselib.ParseMulti1(pd, ctx, pPartLine, parseFlowSemantic)
+}
+func parsePairsSemantic(pd *gparselib.ParseData, ctx interface{}) (*gparselib.ParseData, interface{}) {
+	parts := make([]interface{}, len(pd.SubResults)*2)
+	j := 0
+	for _, subResult := range pd.SubResults {
+		pair := subResult.Value.([]interface{})
+		parts[j] = pair[0]
+		parts[j+1] = pair[1]
+		j += 2
+	}
+	pd.Result.Value = parts
+	return pd, ctx
+}
+func parsePartLineSemantic(pd *gparselib.ParseData, ctx interface{}) (*gparselib.ParseData, interface{}) {
+	partline := []interface{}{
+		pd.SubResults[0].Value,
+	}
+	pairs := pd.SubResults[2].Value.([]interface{})
+	pd.Result.Value = append(partline, pairs...)
+	return pd, ctx
+}
+func parseFlowSemantic(pd *gparselib.ParseData, ctx interface{}) (*gparselib.ParseData, interface{}) {
+	lines := make([][]interface{}, len(pd.SubResults))
+	for i, subResult := range pd.SubResults {
+		line := subResult.Value.([]interface{})
+		lines[i] = line
+	}
+	pd.Result.Value = data.Flow{
+		Parts: lines,
+	}
 	return pd, ctx
 }
